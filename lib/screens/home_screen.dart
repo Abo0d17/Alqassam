b1b506dbbd159d1f1b908e0e419287671a9f9050
Ammar <ammaralabsee@gmail.com>
2024-02-20 11:23:13 +0300

    IOS

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("الرئيسية"),
+      child: Text("كتائب عز الدين القسام"),
     );
   }
 }
